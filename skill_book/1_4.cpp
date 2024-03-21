b400c31858107d1c1b41385ccfa66c5f3dfd5af6
sitrm <dmit_04bur@mail.ru>
2024-03-21 23:41:50 +0300

    completed task 1_4

---
+++ skill_book/1_4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>  // Наибольшее простое число меньше заданного
+
+
+bool is_prime(unsigned int );
+
+
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter number:___\b\b\b";
+    cin >> n;
+    for (int i = n; i >0; i--){
+        if (is_prime(i)){
+            cout << i << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
+
+bool is_prime(unsigned int n){
+    bool flag = true;
+    for (int i=2; i <= n / 2; i++){
+        if (n%i==0){
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
